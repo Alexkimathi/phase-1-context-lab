7466a4268749b04ddf7d70ba3a7db39c258c77e1
alex kimathi <keemalekie@gmail.com>
2022-06-19 12:05:34 +0300

    commit tree b8d1a1668b753b2013c8a65cee16d4975902eb7d

--- index.js
+++ index.js
@@ -1,5 +1,67 @@
 /* Your Code Here */
 
+
+function createEmployeeRecord(array){
+    const records ={
+        firstName: array[0],
+        familyName: array[1],
+        title: array[2],
+        payPerHour: array[3],
+        timeInEvents: [],
+        timeOutEvents: []
+    }
+    return records
+}
+
+function createEmployeeRecords(array){
+    let newArr = []
+    array.forEach(arr=>{
+        newArr.push(createEmployeeRecord(arr))
+    })
+    return newArr
+}
+
+function createTimeInEvent(hrs, dates){
+
+//   time =[
+
+//     {
+//       type: "TimeIn",
+//       hour: "hrs",
+//       date: "dates"
+//     }
+
+//   ]
+//   return time
+
+}
+
+
+function createTimeInEvent(){
+ 
+}
+
+// function hoursWorkedOnDate() {
+
+// }
+
+// function wagesEarnedOnDate(){
+
+// }
+
+// function allWagesFor(){
+//     return allWagesFor
+
+// }
+
+// function findEmployeeByFirstName(srcArray, firstName){
+    
+
+// }
+// function calculatePayroll(){
+
+// }
+
 /*
  We're giving you this function. Take a look at it, you might see some usage
  that's new and different. That's because we're avoiding a well-known, but
